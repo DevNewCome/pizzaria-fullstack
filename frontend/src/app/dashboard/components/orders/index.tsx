9778e581be79ccecee710477979379c50e5de159
Jefferson <jefferson.senac1@yahoo.com>
2025-07-04 21:58:26 -0300

    created a functionality to refresh the order modal

--- frontend/src/app/dashboard/components/orders/index.tsx
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -7,15 +7,22 @@ import { OrderProps } from '@/lib/order.type'
 import { Modalorder } from '../modal'
 import { use } from 'react'
 import { OrderContext } from '@/providers/order'
-
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
 interface Props{
     orders: OrderProps[]
 }
 export function Orders({orders}: Props){
     const {isOpen,onRequestOpen} = use(OrderContext)
+    const router = useRouter()
 
    async function handleDetailOrder(order_id: string){
         await onRequestOpen(order_id)
+    }
+
+    function handleRefreshPage(){
+        router.refresh()
+        toast.success('Pedidos atualizados com sucesso')
     }
 
     return(
@@ -23,11 +30,14 @@ export function Orders({orders}: Props){
         <main className={styles.container}>
             <section className={styles.containerHeader}>
                 <h1>Ultimos pedidos</h1>
-                <button>
+                <button onClick={handleRefreshPage}>
                     <RefreshCw size={24} color="#3fffa3" />
                 </button>
             </section>
             <section className={styles.listOrders}>
+                {orders.length === 0 &&(
+                    <span className={styles.emptyItem}>Nenhum pedido aberto no momento</span>
+                )}
                {orders.map(order => (
                 <button 
                 key={order.id} 
